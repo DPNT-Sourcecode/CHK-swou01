761ed818941b14229edf68343f57d59159ea7afa
sbx_user1051 <sbx_user1051@169.254.7.151>
2025-10-28 10:42:24 +0000

    Tue Oct 28 10:42:24 UTC 2025

--- test/solutions/HLO/test_hlo_solution.js
+++ test/solutions/HLO/test_hlo_solution.js
@@ -5,25 +5,13 @@ var assert = require("assert");
 const SumSolution = require("../../../lib/solutions/SUM/sum_solution");
 
 describe("HLO challenge: greeting the world", function () {
-	it("should return a greeting message with the friend's name", function () {
-		assert.equal(
-			new HelloSolution().hello("Alice"),
-			"Hello, Alice, and hello world!",
-		);
+	it("should return a standard greeting message if a name is provided", function () {
+		assert.equal(new HelloSolution().hello("Alice"), "Hello, World!");
 	});
 
-	it("should return a greeting message with 'friend' when no name is provided", function () {
-		assert.equal(
-			new HelloSolution().hello(),
-			"Hello, friend, and hello world!",
-		);
-	});
-
-	it("should return a greeting message with 'friend' when name is not a string", function () {
-		assert.equal(
-			new HelloSolution().hello(123),
-			"Hello, friend, and hello world!",
-		);
+	it("should return a standard greeting message if a name is not provided", function () {
+		assert.equal(new HelloSolution().hello(), "Hello, World!");
 	});
 });
+
 
