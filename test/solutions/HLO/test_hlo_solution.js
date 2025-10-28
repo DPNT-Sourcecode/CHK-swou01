1cd05d99fb5a70431b2cdb34ebc5cd6bf3141a4d
sbx_user1051 <sbx_user1051@169.254.7.151>
2025-10-28 10:38:34 +0000

    Tue Oct 28 10:38:34 UTC 2025

---
+++ test/solutions/HLO/test_hlo_solution.js
@@ -0,0 +1,14 @@
+var mocha = require("mocha");
+var describe = mocha.describe;
+var it = mocha.it;
+var assert = require("assert");
+const SumSolution = require("../../../lib/solutions/SUM/sum_solution");
+
+describe("HLO challenge: greeting the world", function () {
+	it("should return a greeting message with the friend's name", function () {
+		assert.equal(
+			new HelloSolution().hello("Alice"),
+			"Hello, Alice, and hello world!",
+		);
+	});
+});
